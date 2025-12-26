202686c44fa6164f12bb4dd7859795b6187f5959
vctchinh <23120194@student.hcmus.edu.vn>
2025-12-26 09:02:26 +0700

    Merge: accept remote deletion of dist files

--- src/backend/product/dist/product/src/category/dtos/request/publish-category-request.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { CategoryStatus } from 'src/common/enums';
-export declare class UpdateCategoryStatusRequestDto {
-    categoryId: string;
-    tenantId: string;
-    status: CategoryStatus | string;
-    productApiKey: string;
-}
