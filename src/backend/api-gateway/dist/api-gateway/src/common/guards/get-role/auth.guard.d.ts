ad7edd825de4b55278800d70a920889f7e28fa0d
oppaii230205 <nguyenhungthinha1@gmail.com>
2025-12-26 15:13:52 +0700

    chore: remove dist, node_modules, and build folders from version control

--- src/backend/api-gateway/dist/api-gateway/src/common/guards/get-role/auth.guard.d.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { ClientProxy } from '@nestjs/microservices';
-export declare class AuthGuard implements CanActivate {
-    private readonly identityClient;
-    private readonly configService;
-    constructor(identityClient: ClientProxy, configService: ConfigService);
-    canActivate(context: ExecutionContext): Promise<boolean>;
-}
