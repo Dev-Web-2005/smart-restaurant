0b6d0a7faa96f7256ee20a14d160dceb0a234fec
oppaii230205 <nguyenhungthinha1@gmail.com>
2026-01-11 16:47:19 +0700

    fix: Add CacheModule registration to Cart Module

--- src/backend/order/src/cart/cart.module.ts
+++ src/backend/order/src/cart/cart.module.ts
@@ -1,12 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { CartService } from './cart.service';
 import { CartController } from './cart.controller';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
 	imports: [
 		ConfigModule,
+		// 1. Cấu hình Redis Cache
+		CacheModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				store: await redisStore({
+					socket: {
+						host: configService.get('REDIS_HOST') || 'localhost',
+						port: parseInt(configService.get('REDIS_PORT')) || 6379,
+					},
+					ttl: 86400 * 1000, // Mặc định lưu 24h (tính bằng mili-giây)
+				}),
+			}),
+			inject: [ConfigService],
+		}),
 		ClientsModule.register([
 			{
 				name: 'PRODUCT_SERVICE',
