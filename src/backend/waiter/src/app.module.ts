c5c60cef29a10bd18530795791552a635d54d3b1
oppaii230205 <nguyenhungthinha1@gmail.com>
2026-01-12 08:52:47 +0700

    feat: Set up Producer of MQ in Order Module

--- src/backend/waiter/src/app.module.ts
+++ src/backend/waiter/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WaiterModule } from './waiter/waiter.module';
 
 @Module({
-	imports: [],
+	imports: [WaiterModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
